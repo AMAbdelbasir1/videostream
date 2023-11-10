8966d62e6826c6efe5de6f0a8c70df422f6c6fac
AMAbdelbasir1 <addtooo140@gmail.com>
2023-11-10 21:09:16 +0200

    first commit

---
+++ public/script.js
@@ -0,0 +1,136 @@
+const socket = io();
+socket.on("connect", () => {
+  console.log("client connected");
+});
+const videoGrid = document.getElementById("video-grid");
+const myPeer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+let myVideoStream;
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+const peers = {};
+
+try {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: true })
+    .then((stream) => {
+      myVideoStream = stream;
+      addVideoStream(myVideo, stream);
+
+      myPeer.on("call", (call) => {
+        call.answer(stream);
+        const video = document.createElement("video");
+        call.on("stream", (userVideoStream) => {
+          addVideoStream(video, userVideoStream);
+        });
+      });
+
+      socket.on("user-connected", (userId) => {
+        connectToNewUser(userId, stream);
+      });
+
+      // Rest of your code for handling messages and other functionality
+    })
+    .catch((error) => {
+      console.log("Error accessing camera:", error);
+      // Handle the case where the user doesn't have a camera (e.g., display a message)
+    });
+} catch (error) {
+  console.log("Error accessing camera:", error);
+  // Handle the case where the user doesn't have a camera (e.g., display a message)
+}
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
+
+const scrollToBottom = () => {
+  var d = $(".main__chat_window");
+  d.scrollTop(d.prop("scrollHeight"));
+};
+
+const muteUnmute = () => {
+  if (myVideoStream && myVideoStream.getAudioTracks().length > 0) {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getAudioTracks()[0].enabled = false;
+      setUnmuteButton();
+    } else {
+      setMuteButton();
+      myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+  }
+};
+
+const playStop = () => {
+  if (myVideoStream && myVideoStream.getVideoTracks().length > 0) {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getVideoTracks()[0].enabled = false;
+      setPlayVideo();
+    } else {
+      setStopVideo();
+      myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+  }
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
